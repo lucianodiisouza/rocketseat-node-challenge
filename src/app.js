db79c882c9ef667c942e46254a95540efdc0422c
lucianodiisouza <lucianodiisouza@hotmail.com>
2020-10-31 21:12:45 -0300

    done: post, put, delete, get. remains: update like

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-
-// const { v4: uuid, validate: isUuid } = require('uuid');
+const { v4: uuid, validate: isUuid } = require("uuid");
 
 const app = express();
 
@@ -10,23 +9,71 @@ app.use(cors());
 
 const repositories = [];
 
-app.get("/repositories", (request, response) => {
-  // TODO
+// simple_list - done
+app.get("/repositories", (req, res) => {
+  return res.json(repositories);
 });
 
-app.post("/repositories", (request, response) => {
-  // TODO
+// create - done
+app.post("/repositories", (req, res) => {
+  const { title, url, techs } = req.body;
+
+  const repository = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0,
+  };
+
+  repositories.push(repository);
+
+  return res.json(repository);
 });
 
-app.put("/repositories/:id", (request, response) => {
-  // TODO
+// update - done
+app.put("/repositories/:id", (req, res) => {
+  const { id } = req.params;
+  const { title, url, techs } = req.body;
+
+  const toUpdate = repositories.findIndex((repository) => repository.id === id);
+
+  if (toUpdate < 0) {
+    return res.status(404).json({ error: "Repository not found" });
+  }
+
+  const actualLikes = repositories[toUpdate].likes;
+
+  const repository = {
+    id,
+    title,
+    url,
+    techs,
+    likes: actualLikes,
+  };
+
+  repositories[toUpdate] = repository;
+
+  return res.json(repository);
 });
 
-app.delete("/repositories/:id", (request, response) => {
-  // TODO
+// delete - done
+app.delete("/repositories/:id", (req, res) => {
+  const { id } = req.params;
+
+  const toRemove = repositories.findIndex((repository) => repository.id === id);
+
+  if (toRemove < 0) {
+    return res.status(404).json({ error: "Repository not found" });
+  }
+
+  repositories.splice(toRemove, 1);
+
+  return res.json({ message: "Repository deleted" });
 });
 
-app.post("/repositories/:id/like", (request, response) => {
+// like repo
+app.post("/repositories/:id/like", (req, res) => {
   // TODO
 });
 
